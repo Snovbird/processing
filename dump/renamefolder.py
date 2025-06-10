0a8acfd24a448ec2d482504dda78bcaa1a6de49c
Snovbird <167371033+Snovbird@users.noreply.github.com>
2025-06-10 17:27:56 -0400

    ov

--- dump/renamefolder.py
+++ dump/renamefolder.py
@@ -1,9 +1,14 @@
 
 import os
 import wx
-def renamefromfolder(folderpath,file):
+def rename_file_to_folder_name(folderpath,file,count = 0):
     name, extension = os.path.splitext(file)
-    os.rename(os.path.join(folderpath,file),os.path.join(folderpath,os.path.basename(os.path.dirname(folderpath)) + extension)
+    newfilepath = os.path.join(folderpath,os.path.basename(os.path.dirname(folderpath)) + count.replace('0','')+ extension)
+    if os.path.exists(newfilepath):
+        count +=1
+        rename_file_to_folder_name(folderpath,file,count)
+    os.rename(os.path.join(folderpath,file),newfilepath)
+
 def select_folder(message="Choose a directory containing files:"):
     """Show folder selection dialog and return selected path"""
     with wx.DirDialog(None, message, 
@@ -17,8 +22,9 @@ def main():
     app = wx.App(False)
 
     selectedfolder = select_folder()
+    
     for file in os.listdir(selectedfolder):
         if os.path.isfile(file):
-
+            rename_file_to_folder_name(selectedfolder,file)
 if __name__ == "__main__":
     main()
