3afa10eafa3a3e4cd91549e33fa2e13f10e1dcea
Snovbird <mattscienti@gmail.com>
2025-06-21 23:06:41 -0400

    delfilepy

---
+++ deleteifnot.py
@@ -0,0 +1,13 @@
+import os
+from common.common import select_folder,windowpath,askstring,custom_dialog
+import shutil
+def main():
+    startpath = windowpath()
+    folder_path = select_folder(title="Select FOLDER to delete files inside",path=startpath)
+
+    stringtodelete = askstring("Search for keyword:")
+    ifincluded = custom_dialog(msg="Only delete if this string is INCLUDED or EXCLUDED?",title="File name handling",op1='INCLUDED',op2='EXCLUDED')
+    
+    for i in os.listdir(folder_path):
+        if stringtodelete in i:
+            os.remove(os.path.join(folder_path,i))
