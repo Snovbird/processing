77cedda4fcaa6071e7abbbcf07988be606ca8ae2
Snovbird <mattscienti@gmail.com>
2025-06-21 23:21:35 -0400

    d

--- deleteifnot.py
+++ deleteifnot.py
@@ -6,8 +6,12 @@ def main():
     folder_path = select_folder(title="Select FOLDER to delete files inside",path=startpath)
 
     stringtodelete = askstring("Search for keyword:")
-    ifincluded = custom_dialog(msg="Only delete if this string is INCLUDED or EXCLUDED?",title="File name handling",op1='INCLUDED',op2='EXCLUDED')
+    # ifincluded = custom_dialog(msg="Only delete if this string is INCLUDED or EXCLUDED?",title="File name handling",op1='INCLUDED',op2='EXCLUDED')
     
     for i in os.listdir(folder_path):
-        if stringtodelete in i:
+        if len(i.split('_')) > 2:
             os.remove(os.path.join(folder_path,i))
+        if stringtodelete in i and False:
+            os.remove(os.path.join(folder_path,i))
+
+main()
