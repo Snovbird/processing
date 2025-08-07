776bf730292432672be8256cc05a69c864319d4e
Snovbird <167371033+Snovbird@users.noreply.github.com>
2025-08-07 01:57:20 -0400

    .

--- process_recordings.py
+++ process_recordings.py
@@ -55,11 +55,11 @@ def process_folder():
         
         # Concatenations variables (needed for photo carrousel)
         concatenation_output_folder = makefolder(grouped_files[0][0], foldername='(delete me once done) Gradually processed videos')
-        ready_combined_imgs_paths = []
+        ready_combined_imgs_paths = {}
         
         # Photo carroussel to verify if overlays aren't displaced
-        combined_output_folder = makefolder(concatenation_output_folder, foldername='combined')
-        png_outputs = makefolder(combined_output_folder, foldername='png')
+        png_outputs = makefolder(concatenation_output_folder, foldername='png')
+        combined_output_folder = makefolder(png_outputs, foldername='combined')
         for group in grouped_files:
             bg_imgpath = extractpng(group[0],times=[1],output_folder=png_outputs)[0]
             date_for_group = os.path.splitext(os.path.basename(group[0]))[0].split("_")[1]
@@ -84,26 +84,7 @@ def process_folder():
         if not grouped_files:
             error("No files found that can be grouped for concatenation.")
             return
-        
-        # Concatenations variables (needed for photo carrousel)
-        concatenation_output_folder = makefolder(grouped_files[0][0], foldername='(delete me once done) Gradually processed videos')
-        ready_combined_imgs_paths = {}
-        
-        # Photo carroussel to verify if overlays aren't displaced
-        combined_output_folder = makefolder(concatenation_output_folder, foldername='combined')
-        png_outputs = makefolder(combined_output_folder, foldername='png')
-        for group in grouped_files:
-            bg_imgpath = extractpng(group[0],times=[1],output_folder=png_outputs)[0]
-            date_for_group = os.path.splitext(os.path.basename(group[0]))[0].split("_")[1]
-            cage_number = ''.join(char for char in os.path.splitext(os.path.basename(group[0]))[0][0:2] if char.isdigit()) # extract digits from first two filename characters to get cage number
-            overlay_imgpath = find_imgpath_overlay_date(date_provided=date_for_group,room=room,cage_number=cage_number)
-            combined_outputpath = combine_and_resize_images(bg_imgpath,overlay_imgpath,output_folder=combined_output_folder)
-            ready_combined_imgs_paths[combined_outputpath] = cage_number
-
-        for imgpath, number in ready_combined_imgs_paths.items():
-            if photo_carrousel(imgpath) == 'STOP markers NOT aligned':
-                return emergency_overlay_maker(cage_number=number,room=room)
-
+                
         # Concatenate each group of videos
         for group in grouped_files:
             combine_videos_with_cuda(group, concatenation_output_folder)
@@ -138,8 +119,8 @@ def process_folder():
         import time
         while True:
             try:
-                os.remove(concatenation_output_folder)
                 print("Trying to DELETE")
+                os.remove(concatenation_output_folder)
                 break
             except:
                 time.sleep(1)
