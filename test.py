386a428ff201a5f70cd78f0852171d0c54bab72d
Snovbird <mattscienti@gmail.com>
2025-08-12 15:11:39 -0400

    .

--- test.py
+++ test.py
@@ -1,5 +1,25 @@
-from common.common import dropdown,msgbox()
+from common.common import dropdown,msgbox
 import os
+start_time = 20
+list_of_timestamps = [start_time]
+def div(arg1,arg2):
+    try:
+        return arg1 / arg2
+    except ZeroDivisionError:
+        return 0
 
-msgbox(msg="Video Processing complete!",title="Success")
-os.startfile(processed_outputfolder)
+for i in range(10): # 10*3 = 30 times to go through
+    for ITI_len in [40,80,150]:
+        start_time += ITI_len + 40
+        list_of_timestamps.append(start_time)
+del list_of_timestamps[-1]
+print(list_of_timestamps)
+Cycle = [i for i in range(30)]
+ITIlist = [int(list_of_timestamps[i]+ 40*(1 if div(i% 2, i% 2) != 0 else -0.25)) for i in range(1,len(Cycle)-1)]
+print(ITIlist)
+
+ITI_start = [i for c,i in enumerate(ITIlist) if c % 2 == 0]
+ITI_end = [i for c, i in enumerate(ITIlist) if c % 2 != 0]
+print(f"{ITI_start=}\t{len(ITI_start)}")
+print(f"{ITI_end=}\t{len(ITI_end)}")
+
--- trial_formula.py
+++ trial_formula.py
@@ -10,7 +10,6 @@ def trial_formula(plus_or_minus_first:str|None = None,extract_which:str|None=Non
     start_time = askint(msg="Enter Start start time:",title="Start time",fill=findval("start_time"))
     assignval("start_time",start_time)
     list_of_timestamps = [start_time]
-    e = 1
     for i in range(10): # 10*3 = 30 times to go through
         for ITI_len in [40,80,150]:
             start_time += ITI_len + 40
@@ -52,7 +51,7 @@ def trial_formula(plus_or_minus_first:str|None = None,extract_which:str|None=Non
     print(len([i for i in Cycle if i == "DS-"]),"DS- len")
     dspluslist = [list_of_timestamps[i] for i, ds in enumerate(Cycle) if ds == "DS+"]
     dsminuslist = [list_of_timestamps[i] for i, ds in enumerate(Cycle) if ds == "DS-"]
-    ITIlist = [list_of_timestamps[i]+ 40*(1 if div(i% 2, i% 2) != 0 else 0) for i in range(1,len(Cycle))]
+    ITIlist = [int(list_of_timestamps[i]+ 40*(1 if div(i% 2, i% 2) != 0 else -0.25)) for i in range(1,len(Cycle)-1)]
     if not extract_which:
         extract_which = custom_dialog(msg="GET VALUES FOR WHICH CUE?\n This will give timestamps for: DS+, ITI or DS-",title="First trial",op1="DS+",op2="ITI",op3="DS-") # = "DS+" # Timestamps for which of the two cue lights (plus or minus) will be copied
 
@@ -63,11 +62,13 @@ def trial_formula(plus_or_minus_first:str|None = None,extract_which:str|None=Non
         ITIchoice =custom_dialog("start_times or end times","Timestamps","start","end") 
         if ITIchoice == "start":
             return ".".join([f'{hh//3600:01d}{(hh%3600)//60:02d}{hh%60:02d}' for number, hh in enumerate(ITIlist) if number % 2 == 0])
+        
         elif ITIchoice == "end":
             return ".".join([f'{hh//3600:01d}{(hh%3600)//60:02d}{hh%60:02d}' for number, hh in enumerate(ITIlist) if number % 2 != 0])
+        
     elif extract_which == "DS-":
         print("dsminus")
-        return ".".join([f'{hh//3600:01d}{(hh%3600)//60:02d}{hh%60:02d}' for number, hh in enumerate(dsminuslist)]) # if number % 2 != 0
+        return ".".join([f'{hh//3600:01d}{(hh%3600)//60:02d}{hh%60:02d}' for number, hh in enumerate(dsminuslist)]) 
 
 def main():
     import pyperclip
