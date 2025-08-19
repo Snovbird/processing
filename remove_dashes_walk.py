67d1ef3f9c932089af48b9d7ff1ef391389d0f38
Snovbird <167371033+Snovbird@users.noreply.github.com>
2025-08-19 14:23:06 -0400

    .

--- remove_dashes_walk.py
+++ remove_dashes_walk.py
@@ -1,20 +1,27 @@
 import os,shutil
-from common.common import select_folder
+from common.common import select_folder,msgbox
 
 def simple_file_walk_renamer(folder):
     """
     walks through all files in a folder
     """
+    renamed = 0
     for root, dirs, files in os.walk(folder):
         for file in files:
+            msgbox(file)
             if "_" in file:
+                
                 newname = file.replace("_",'-')
                 os.rename(os.path.join(root, file), os.path.join(root, newname))
+                renamed += 1
+    return renamed
 
 # Usage
 
 folder_path = select_folder("Enter folder path: ").strip().strip('"\'')
-simple_file_walk_renamer(folder_path)
+c = simple_file_walk_renamer(folder_path)
+
+msgbox(f"Renamed {c} files")
 
 
 
