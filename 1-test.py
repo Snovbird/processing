91a34481054fe6bf15aedbc0971906a0771a8a5b
Snovbird <167371033+Snovbird@users.noreply.github.com>
2025-08-25 12:16:44 -0400

    .

--- 1-test.py
+++ 1-test.py
@@ -1,9 +1,12 @@
 import pandas,os
-from common.common import assignval,list_folders,list_folderspaths,makefolder,simple_file_walk,msgbox
-
-def thefunc(file):
-    dir = os.path.dirname(file)
-    newname = os.path.basename(file).replace("Annotated video","Annotated video.avi").replace("2-FN-light","FNCL").replace('3-FF-light','FFCL').replace('4-BF-light','BFCL').replace('5-BN-light','BNCL')
-    os.rename(file,os.path.join(dir,newname))
-
-msgbox(f"{simple_file_walk(r"C:\Users\samahalabo\Desktop\9-ANALYSIS\detector test",thefunc)} files renamed")
+from common.common import assignval,list_folders,list_folderspaths,makefolder,simple_file_walk,msgbox,select_video
+def main():
+    a = []
+    while True:
+        choice = [f"'{i.replace('\\','/')}'" for i in select_video()]
+        if not choice:
+            break
+        a.append(choice)
+    import pyperclip
+    pyperclip.copy(a)
+main()
--- foldermaker.pyw
+++ foldermaker.pyw
@@ -2,11 +2,14 @@ import os
 from common.common import find_folder_path,custom_dialog,list_folders
 def main():
     clipspath = find_folder_path("5-clips")
+
     op1, op2, *_ = list_folders(clipspath)
-    choice = custom_dialog("Select folders for which cue?")
+    choice = custom_dialog("Select folders for which cue?","Cue","DS+","DS-")
     if not choice:
         return
     dir6 = find_folder_path("6-GENERATED EXAMPLES")
     for folder in [folder for folder in os.listdir(os.path.join(clipspath,choice)) if os.path.isdir(os.path.join(clipspath,choice, folder))]:
         if folder != "no idea":
             os.makedirs(os.path.join(dir6,folder),exist_ok=True)
+
+main()
